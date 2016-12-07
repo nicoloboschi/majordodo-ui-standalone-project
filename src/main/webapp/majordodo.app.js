f4b81b6bd40178271278cd4e039427359305fee8
Nicolò Boschi <nicolo.boschi@diennea.com>
2016-12-07 14:46:17 +0100

    Search improvement

--- src/main/webapp/majordodo.app.js
+++ src/main/webapp/majordodo.app.js
@@ -1,7 +1,4 @@
 
-
-
-
 var modulo = angular.module('MajordodoWebUI', ['ngRoute']);
 
 modulo.factory('$state', function () {
@@ -10,17 +7,17 @@ modulo.factory('$state', function () {
     return state;
 });
 
-modulo.config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/home',
-                {templateUrl: 'index_1.html', controller: MajordodoHomeCtrl});
-        $routeProvider.when('/workers',
-                {templateUrl: 'workers.html', controller: MajordodoWorkersCtrl});
-        $routeProvider.when('/search/:userid/:tasktype/:status/:slot', //fare filtri in html per bottoni in table
-                {templateUrl: 'search.html', controller: genericSearchCtrl});
-        $routeProvider.when('/resources',
-                {templateUrl: 'resources.html', controller: MajordodoResourcesCtrl});
-        $routeProvider.otherwise({redirectTo: '/home'});
-    }]);
+modulo.config(function ($routeProvider) {
+    $routeProvider.when('/home',
+            {templateUrl: 'index_1.html', controller: MajordodoHomeCtrl});
+    $routeProvider.when('/workers',
+            {templateUrl: 'workers.html', controller: MajordodoWorkersCtrl});
+    $routeProvider.when('/search',
+            {templateUrl: 'search.html', controller: genericSearchCtrl});
+    $routeProvider.when('/resources',
+            {templateUrl: 'resources.html', controller: MajordodoResourcesCtrl});
+    $routeProvider.otherwise({redirectTo: '/home'});
+});
 
 function MajordodoHomeCtrl($scope, $http, $route, $timeout, $location, $state) {
 
@@ -149,12 +146,8 @@ function MajordodoWorkersCtrl($scope, $http, $route, $timeout, $location, $state
         return colors;
     }
 }
-function genericSearchCtrl($scope, $http, $route, $timeout, $location, $state, $routeParams) {
-    var status = $routeParams.status;
-    var userid = $routeParams.userid;
-    var tasktype = $routeParams.tasktype;
-    var slot = $routeParams.slot;
-
+function genericSearchCtrl($scope, $http, $route, $timeout, $location, $state) {
+    var allSearchFilter = ['searchStatus', 'searchWorkerId', 'searchTasktype', 'searchTaskId', 'searchSlot', 'searchUserId'];
     $scope.brokerUrl;
     if (!$state.brokerUrl) {
         $scope.brokerUrl = $location.search().brokerUrl;
@@ -163,40 +156,39 @@ function genericSearchCtrl($scope, $http, $route, $timeout, $location, $state, $
         $scope.brokerUrl = $state.brokerUrl;
     }
     $scope.tasks = [];
-    $scope.filterUserId = '';
-    $scope.filterStatus = '';
-    $scope.filterSlot = '';
-    $scope.filterWorkerId = '';
-    $scope.filterTasktype = '';
-    $scope.filterTaskId = '';
-    $scope.max = '100';
+    $scope.filters = [];
+    $scope.max = '150';
     $scope.lastupdate;
     $scope.lastUrl = '';
     $scope.lastCount = '';
 
-    if (status != "none") {
-        $scope.filterStatus = status;
+    $scope.searchThis = function (id, idsearch) {
+        $('#' + idsearch + "-input").val(id);
+        for (var i = 0, max = allSearchFilter.length; i < max; i++) {
+            var $valore = $('#' + allSearchFilter[i] + "-input").val();
+            $scope.filters[allSearchFilter[i]] = $valore;
+        }
+        toogleClassSuccess(idsearch);
+        $scope.reloadData();
     }
-    if (userid != "none") {
-        $scope.filterUserId = userid;
-    }
-    if (tasktype != "none") {
-        $scope.tasktype = tasktype;
-    }
-    if (slot != "none") {
-        $scope.filterSlot = slot;
-    }
-
     $scope.keyPress = function (event) {
         if (event.keyCode == 13) {
+            for (var i = 0, max = allSearchFilter.length; i < max; i++) {
+                var $valore = $('#' + allSearchFilter[i] + "-input").val();
+                $scope.filters[allSearchFilter[i]] = $valore;
+            }
             $scope.reloadData();
         }
     }
     $scope.go = function (path) {
         $location.path(path);
     };
-
-    $scope.refreshPage = function () {
+    $scope.clearFilters = function () {
+        for (var i = 0; i < allSearchFilter.length; i++) {
+            $('#' + allSearchFilter[i] + "-input").val("");
+              $scope.filters[allSearchFilter[i]] = "";
+              toogleClassSuccess(allSearchFilter[i]);
+        }  
         $scope.reloadData();
     };
 
@@ -205,7 +197,7 @@ function genericSearchCtrl($scope, $http, $route, $timeout, $location, $state, $
     };
 
     if ($scope.brokerUrl) {
-        $timeout($scope.refreshPage, 1000);
+        $timeout($scope.reloadData(), 1000);
     }
 
     $scope.reloadData = function () {
@@ -215,30 +207,31 @@ function genericSearchCtrl($scope, $http, $route, $timeout, $location, $state, $
         $state.brokerUrl = $scope.brokerUrl;
 
         var theUrl = '';
-        if ($scope.filterTaskId) {
-            theUrl = $scope.brokerUrl + '&view=task' + '&taskId=' + encodeURIComponent($scope.filterTaskId);
+        if ($scope.filters['searchTaskId']) {
+            theUrl = $scope.brokerUrl + '&view=task' + '&taskId=' + encodeURIComponent($scope.filters['searchTaskId']);
+            $scope.filters = [];
         } else {
             theUrl = $scope.brokerUrl + '&view=tasks';
-            if ($scope.filterUserId) {
-                theUrl = theUrl + '&userId=' + encodeURIComponent($scope.filterUserId);
+            if ($scope.filters['searchUserId']) {
+                theUrl = theUrl + '&userId=' + encodeURIComponent($scope.filters['searchUserId']);
             }
-            if ($scope.filterStatus) {
-                theUrl = theUrl + '&status=' + encodeURIComponent($scope.filterStatus);
+            if ($scope.filters['searchStatus']) {
+                theUrl = theUrl + '&status=' + encodeURIComponent($scope.filters['searchStatus']);
             }
-            if ($scope.filterSlot) {
-                theUrl = theUrl + '&slot=' + encodeURIComponent($scope.filterSlot);
+            if ($scope.filters['searchSlot']) {
+                theUrl = theUrl + '&slot=' + encodeURIComponent($scope.filters['searchSlot']);
             }
-            if ($scope.filterWorkerId) {
-                theUrl = theUrl + '&workerId=' + encodeURIComponent($scope.filterWorkerId);
+            if ($scope.filters['searchWorkerId']) {
+                theUrl = theUrl + '&workerId=' + encodeURIComponent($scope.filters['searchWorkerId']);
             }
-            if ($scope.filterTasktype) {
-                theUrl = theUrl + '&tasktype=' + encodeURIComponent($scope.filterTasktype);
+            if ($scope.filters['searchTasktype']) {
+                theUrl = theUrl + '&tasktype=' + encodeURIComponent($scope.filters['searchTasktype']);
             }
             if ($scope.max) {
                 theUrl = theUrl + '&max=' + encodeURIComponent($scope.max);
             }
+            $scope.filters['searchTaskId'] = "";
         }
-
         $http.get(theUrl).
                 success(function (data, status, headers, config) {
                     $scope.lastUrl = theUrl;
@@ -274,8 +267,21 @@ function genericSearchCtrl($scope, $http, $route, $timeout, $location, $state, $
         }
         return colors;
     }
-}
 
+}
+function toogleClassSuccess(id) {
+    var $el = $('#' + id);
+    var value = $('#' + id + "-input").val();
+    var classe = $el.attr('class');
+    if (value.length == 0) {
+        $el.attr('class', classe.replace("has-success", ""));
+    } else {
+        if (!classe.includes('has-success')) {
+            $el.attr('class', classe + ' has-success');
+        }
+    }
+
+}
 
 function MajordodoSlotsCtrl($scope, $http, $route, $timeout, $location, $state) {
     $scope.brokerUrl;
@@ -486,3 +492,4 @@ function MajordodoWorkerCtrl($scope, $http, $route, $timeout, $location, $state)
 
     };
 }
+
